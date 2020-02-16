6e4077b9874e4e3772afabb91450bfece8c1989d
Langat <Langat>
2020-02-16 11:41:00 +0300

    debu

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,30 +1,16 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
-import {  throwError } from 'rxjs';
-import { retry, catchError } from 'rxjs/operators';
-@Injectable({
-  providedIn: 'root',
-})
-export class ApiService {
+import { HttpClient } from '@angular/common/http';
 
-  private SERVER_URL = "http://localhost:3000";
+@Injectable({
+  providedIn: 'root'
+})
+export class DataService {
+
+  private FOLLOW_ME = "https://www.youtube.com/channel/UCGehE3r1Udmr2rulMhdhLYQ?view_as=subscriber";
 
   constructor(private httpClient: HttpClient) { }
 
-  handleError(error: HttpErrorResponse) {
-    let errorMessage = 'Unknown error!';
-    if (error.error instanceof ErrorEvent) {
-      // Client-side errors
-      errorMessage = `Error: ${error.error.message}`;
-    } else {
-      // Server-side errors
-      errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
-    }
-    window.alert(errorMessage);
-    return throwError(errorMessage);
+  public fetch(){
+    return this.httpClient.get(this.FOLLOW_ME);
   }
-
-  public get(){  
-		return this.httpClient.get(this.SERVER_URL).pipe(catchError(this.handleError));  
-	}  
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,17 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-import { ApiService } from '../api.service';
+import { DataService } from '../api.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-	products = [];
-	constructor(private apiService: ApiService) { }
-	ngOnInit() {
-		this.apiService.get().subscribe((data: any[])=>{  
-			console.log(data);  
-			this.products = data;  
-		})  
-	}
+
+  entries = [];
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+
+    this.dataService.fetch().subscribe((data: any[])=>{
+      console.log(data);
+      this.entries = data;
+    })  
+  }
+
 }
